b9dff2f558a84ce04ae79d3c0ab7c4ad352e920c
artimis <parisa.safaryazdi@gmail.com>
2021-03-09 19:47:30 +0330

    drop raws with NaN cell

--- clean-data.py
+++ clean-data.py
@@ -1,4 +1,11 @@
 import pandas as pd
-df = pd.read_csv('airtravel.csv')
-print(df.dtypes)
-print(df.describe())
+import numpy as np
+df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-D'
+                 'A0101EN-SkillsNetwork/labs/Data%20files/auto.csv', header=None)
+header = ["id", "name", "price", "id", "name", "value", "id", "name", "value","id", "name", "value","id", "name", "value", "id", "name", "value", "id", "name", "value","id", "name", "value","yekimande","akhar"]
+df.columns = header
+df1 = df.replace('?', np.NaN)
+df = df1.dropna(subset = ['price'] , axis = 0)
+print(df1.dtypes)
+print(df1.describe(include = "all"))
+print(df1.info())
